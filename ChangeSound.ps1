e06cbff8268710615b244896d93c925dc0a709b0
Bill Schroeder <bill@zealocity.com>
2022-06-24 16:45:49 -0500

    Add files via upload

---
+++ ChangeSound.ps1
@@ -0,0 +1,10 @@
+﻿#Install-Module -Name AudioDeviceCmdlets -Force -Verbose  
+#get-module -Name "AudioDeviceCmdlets" -ListAvailable | Sort-Object Version | select -last 1 | Import-Module -Verbose
+
+
+#Get-AudioDevice -list
+#Get-AudioDevice -List | where Type -like "Playback"
+#Get-AudioDevice -List | where Type -like "Playback" | where name -like "*Speakers (Realtek(R) Audio)*" 
+#Get-AudioDevice -List | where Type -like "Playback" | where index -like "1"
+
+Get-AudioDevice -List | where Type -like "Playback" | where index -like "1"  | Set-AudioDevice -Verbose
